b261c818310ad8df974d900a4c402646d59ca870
IlianaPinto <iliana.pinto04@gmail.com>
2020-09-17 00:56:05 -0600

    item schema and router finished

--- server/models/Item.js
+++ server/models/Item.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const Item = new Schema({
+    userID: String,
     name: String,
     value: Number,
     year: Number,
--- server/routes/items.js
+++ server/routes/items.js
@@ -4,7 +4,30 @@ const router = express.Router();
 const Item = require('../models/Item');
 
 router.get('/', async (req,res) => {
-    const item = await Item.find();
+    const items = await Item.find(req.params.userID);
+    res.json(items);
 });
+
+router.post('/', async (req, res) => {
+    const item = new Item(req.body);
+    await item.save();
+    res.json({
+        staus: 'Item saved!',
+    });
+})
+
+router.put('/:id', async (req, res) => {
+    await Item.findByIdAndUpdate(req.params.id, req.body);
+    res.json({
+        status: "Item updated!",
+    })
+})
+
+router.delete('/:id', async (req, res) => {
+    await Item.findByIdAndRemove(req.params.id);
+    res.json({
+        status: "Item deleted",
+    })
+})
 
 module.exports = router;
