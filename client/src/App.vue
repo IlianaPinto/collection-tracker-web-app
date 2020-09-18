f8713c70f1cfc8f59219a2779d6ebfaf1d553580
IlianaPinto <iliana.pinto04@gmail.com>
2020-09-18 16:06:30 -0600

    navbar ready

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/"> El home </router-link>
-      <router-link to="/collections"> El collections </router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark nav_color">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#">Collector Tracker</a>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/collections">My Collection <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <button type="button" class="btn btn-success">Log in</button>
+        </form>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
 
-<script>
-//import Home from './components/Home.vue'
-
-export default {
-  name: 'App',
-  /*components: {
-    Home
-  }*/
-}
-</script>
-
 <style>
-
+  .nav_color{
+    background: #0f0c29;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
 </style>
